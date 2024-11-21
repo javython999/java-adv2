8e5a5f02bab731089a3420458886ae508f41b91f
javython999 <infinity2544@gmail.com>
2024-11-21 20:52:09 +0900

    HttpRequest, HttpResponse

---
+++ src/was/httpserver/HttpReqeust.java
@@ -0,0 +1,94 @@
+package was.httpserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.*;
+
+public class HttpReqeust {
+
+    private String method;
+    private String path;
+    private final Map<String, String> queryParameters = new HashMap<>();
+    private final Map<String, String> headers = new HashMap<>();
+
+    public HttpReqeust(BufferedReader reader) throws IOException {
+        parseRequestLine(reader);
+        parseHeaders(reader);
+    }
+
+
+    private void parseRequestLine(BufferedReader reader) throws IOException {
+        String requestLine = reader.readLine();
+        if (requestLine == null) {
+            throw new IOException("EOF: No request line received");
+        }
+
+        String[] parts = requestLine.split(" ");
+        if (parts.length != 3) {
+            throw new IOException("Invalid request line: " + requestLine);
+        }
+
+        method = parts[0];
+        String[] pathsParts = parts[1].split("\\?");
+        path = pathsParts[0];
+        if (pathsParts.length > 1) {
+            parseQueryParameters(pathsParts[1]);
+        }
+    }
+
+    private void parseQueryParameters(String queryString) {
+        for (String param : queryString.split("&")) {
+            String[] keyValue = param.split("=");
+            String key = URLDecoder.decode(keyValue[0], UTF_8);
+            String value = keyValue.length > 1 ? URLDecoder.decode(keyValue[1], UTF_8) : "";
+            queryParameters.put(key, value);
+        }
+    }
+
+    private void parseHeaders(BufferedReader reader) throws IOException {
+        String line;
+        while (!(line = reader.readLine()).isEmpty()) {
+            String[] headerParts = line.split(":");
+            headers.put(headerParts[0].trim(), headerParts[1].trim());
+        }
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getParameter(String name) {
+        return queryParameters.get(name);
+    }
+
+    public String getHeader(String name) {
+        return headers.get(name);
+    }
+
+    @Override
+    public String toString() {
+        return "HttpReqeust{" +
+                "method='" + method + '\'' +
+                ", path='" + path + '\'' +
+                ", queryParameters=" + queryParameters +
+                ", headers=" + headers +
+                '}';
+    }
+}
